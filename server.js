ccb0fd31cc2917ca3266c0fe2b5bf6804d67de1e
Patty C <patrickcorcoran10@gmail.com>
2018-10-10 21:41:19 -0500

    Commit connecting the app with MONGODB_URI

--- server.js
+++ server.js
@@ -29,9 +29,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/newsApp", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost/newsApp", { useNewUrlParser: true });
 
 var express = require("express");
+// mongoose.Promise = Promise;
+// mongoose.connect(MONGODB_URI);
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newsApp";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 // Scraping Function
